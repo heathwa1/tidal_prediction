885db983aa244b0d953a26e6bff4f14d041c1747
heathwa1 <heathwa1@uw.edu>
2021-06-05 11:43:40 -0700

    test

--- javascript.js
+++ javascript.js
@@ -8,144 +8,39 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiaC13YXJyZW4iLCJhIjoiY2tpbmV2aHl5MDYxcjJzcGNqaGJzNWwyNSJ9.8Iik9-Klh7z3K-zMi-m_yg'
 }).addTo(map);
   //add empty feature group for drawn items to be placed in
-var tidalStations = L.featureGroup().addTo(map);
-  //hold data already in carto table
-// var cartoData = L.layerGroup().addTo(map);
-var tidalStations = "waterlevel_prediction_stations2.json"
+// var tidalStations = L.featureGroup().addTo(map);
 
-$.getJSON("waterlevel_prediction_stations2.json", function(data) {
-    var stations = L.geoJson(data, {
-
-      pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-          color: 'red',
-          radius:5
-        });
-      },
-    }).addTo(map);
+L.esri.get(url:'https://idpgis.ncep.noaa.gov/arcgis/rest/services/NOS_Observations/CO_OPS_Products/FeatureServer/6', {}, function (error, response) {
+  if (error) {
+    return;
   });
 
-
-var stations = turf.featureCollection(tidalStations.features);
-var points = turf.point(tidalStations.geometry.coordinates);
-
-  map.on("click", function selectLocationOnMap(event) {
-    var coords = [event.latlng.lng, event.latlng.lat];
-    alert(coords);
-  })
-  function nearest(e) {
-    var userClick = turf.getCoord(coords);
-    var tidal = station.properties.id;
-    nearStation = turf.nearestPoint(tidal, coords);
-
-
-  console.log(nearStation);
-    geojson.addTo(map);
-}
-//   function updateCoordsToFind(coords, locate = false) {
-//   coordsToFind = coords;
-// }
+//   var features = response.operationalLayers[0].featureCollection.layers[0].featureSet.features;
+//   var idField = response.operationalLayers[0].featureCollection.layers[0].layerDefinition.objectIdField;
 //
-// let featureTypeToFind = sqlQuery();
-// let typeName = getFeatureTypeToFind(featureTypeToFind);
-//   var circle = turf.circle(coordsToFind, 1, {units: "miles"});
-//   circle.turf.flip(circle);
-
-//   var closeTides = turf.nearestPoint(circle, sqlQuery, {units: "miles"});
-//   closeTides = turf.flip(closeTides);
+//   // empty geojson feature collection
+//   var featureCollection = {
+//     type: 'FeatureCollection',
+//     features: []
+//   };
 //
-// function fetchWhile(resultsRemain) {
-//   fetch(url(sqlQuery))
-//   .then((response) = response.json())
-//   .then((data) => {
-//     sqlQuery.startIndex += sqlQuery.count;
-// geojson.features.push.apply(geojson.features, data.features);
-// resultsRemain = data.features.length < sqlQuery.count ? false: true;
-// fetchWhile(resultsRemain);
+//   for (var i = features.length - 1; i >= 0; i--) {
+//     // convert ArcGIS Feature to GeoJSON Feature
+//     var feature = L.esri.Util.arcgisToGeoJSON(features[i], idField);
 //
-// })
-// .catch((err) =>  {
-//   console.error(err)
-// });
-// }
-// }
-// else {
-//   removerSpinner();
-//   if (geojson.features.length) {
-//     return closeTides(pointToFind, geojson, 20, typeName);
+//     // unproject the web mercator coordinates to lat/lng
+//     var latlng = L.Projection.Mercator.unproject(L.point(feature.geometry.coordinates));
+//     feature.geometry.coordinates = [latlng.lng, latlng.lat];
 //
-//   } else {
-//     notification.show("error", "No features found");
+//     featureCollection.features.push(feature);
 //   }
-// }
-
-  // var coords = closeTides.geometry.coordinates[0].join(" ");
-
-
-      //find lat lng from user click
-//  map.on('click',function(e) {
-//     var tideLoc = new L.marker(e.latlng, )
+//
+//   var geojson = L.geoJSON(featureCollection).addTo(map);
+//   map.fitBounds(geojson.getBounds());
 // });
-//
-//     //on click lat lng find nearest
-//
-// new L.Control.Draw({
-//     draw: {
-//         polygon: false,
-//         polyline: false,
-//         rectangle: false,
-//         circle: false,
-//         circlemarker: false,
-//         marker: true
-//     },
-//     edit: {
-//         featureGroup: drawnItems
-//     }
-// }).addTo(map);
-//
-// function createFormPopup() {
-//     var popupContent =
-//       '<form>' +
-//       'Location name:<br><input type="text" id="input_name"><br>' +
-//       'Description:<br><input type="text" id="input_desc"><br>' +
-//       '<input type="button" value="Submit" id="submit">' +
-//       '</form>'
-//     drawnItems.bindPopup(popupContent).openPopup();
-// }
-//
-// map.addEventListener("draw:created", function(e) {
-//     e.layer.addTo(drawnItems);
-//     createFormPopup();
-//     });
-//
-// function setData(e) {
-//     if(e.target && e.target.id == 'submit') {
-//       //get name and description from popup form
-//       var enteredLocName = document.getElementById("input_name").value;
-//       var enteredDescription = document.getElementById("input_desc").value;
 
-  //send drawn layers data to carto database;
 
-      //for each drawn layer on the site map
-    // drawnItems.eachLayer(function(layer) {
-    //   //create SQL expression to insert layer;
-    //   var drawing = JSON.stringify(layer.toGeoJSON().geometry);
-    //         var sql =
-    //             "INSERT INTO lab_3b_heather (the_geom, name, description) " +
-    //             "VALUES (ST_SetSRID(ST_GeomFromGeoJSON('" +
-    //             drawing + "'), 4326), '" +
-    //             enteredLocName + "', '" +
-    //             enteredDescription + "')";
-    //         console.log(sql);
 
-      //send data to carto with CARTO SQL API
-//           fetch(url, {
-//               method: "POST",
-//               headers: {
-//                   "Content_Type": "application/x-www-form-urlencoded"
-//               },
-//               body: "q=" + encodeURI(sql)
-//           })
 //           .then(function(response) {
 //               return response.json();
 //           })
